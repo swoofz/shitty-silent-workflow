61a55de720b1609134b6c252527fea8e025838b7
Jordan Hall <jordanhall.1995@yahoo.com>
2017-05-02 17:35:53 -0700

    home route create

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require("express")
 var app = express()
 
+app.get('/', function(req, res) {
+  res.send("This is the home page")
+})
+
 app.listen(3000, function () {
   console.log("Listening on port 3000.");
 })
